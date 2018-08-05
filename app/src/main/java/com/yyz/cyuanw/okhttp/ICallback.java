87a0394771a508fad375d8ec8b79e683d74ab221
yyz12121000 <247650745@qq.com>
2018-08-05 18:16:40 +0800

    网络框架OKhttp3部分代码2

--- app/src/main/java/com/yyz/cyuanw/okhttp/ICallback.java
+++ app/src/main/java/com/yyz/cyuanw/okhttp/ICallback.java
@@ -1,12 +1,13 @@
-//package com.yyz.cyuanw.okhttp;
-//
-//import java.io.IOException;
-//
-//
-//
-//
-//public interface ICallback {
-//	void onResponse(Response response) throws IOException;
-//	void onFailure(Request arg0, Exception arg1);
-//}
-//
+package com.yyz.cyuanw.okhttp;
+
+        import java.io.IOException;
+
+        import okhttp3.Request;
+        import okhttp3.Response;
+
+
+public interface ICallback {
+    void onResponse(Response response) throws IOException;
+    void onFailure(Request arg0, Exception arg1);
+}
+
