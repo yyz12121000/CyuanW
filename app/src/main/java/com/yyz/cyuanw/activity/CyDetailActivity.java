09641f2f3c234888a14d1622e34262c95f1b1efb
yyz12121000 <247650745@qq.com>
2018-08-14 10:02:41 +0800

    优化车源详情UI

--- app/src/main/java/com/yyz/cyuanw/activity/CyDetailActivity.java
+++ app/src/main/java/com/yyz/cyuanw/activity/CyDetailActivity.java
@@ -1,5 +1,7 @@
 package com.yyz.cyuanw.activity;
 
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.yyz.cyuanw.R;
@@ -9,6 +11,8 @@ import butterknife.BindView;
 public class CyDetailActivity extends BaseActivity {
     @BindView(R.id.id_tv_title)
     TextView title;
+    @BindView(R.id.title_right_icon)
+    ImageView right_icon;
     @Override
     protected int getLayoutId() {
         return R.layout.activity_cy_detail;
@@ -17,6 +21,8 @@ public class CyDetailActivity extends BaseActivity {
     @Override
     public void initView() {
         title.setText("车源详情");
+        right_icon.setImageResource(R.mipmap.img_25);
+        right_icon.setVisibility(View.VISIBLE);
     }
 
     @Override
