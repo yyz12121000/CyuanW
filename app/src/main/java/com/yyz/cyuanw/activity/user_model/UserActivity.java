7439abe3a52fc6c02bdcb36705779d16f28745b0
Huangjin <644651872@qq.com>
2018-08-15 17:21:56 +0800

    glide圆形图片带边框，签名文件

--- app/src/main/java/com/yyz/cyuanw/activity/user_model/UserActivity.java
+++ app/src/main/java/com/yyz/cyuanw/activity/user_model/UserActivity.java
@@ -2,13 +2,18 @@ package com.yyz.cyuanw.activity.user_model;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.yyz.cyuanw.R;
 import com.yyz.cyuanw.activity.BaseActivity;
+import com.yyz.cyuanw.tools.GlideCircleTransformWithBorder;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -16,6 +21,7 @@ import butterknife.OnClick;
 public class UserActivity extends BaseActivity{
 
     @BindView(R.id.id_iv_back) ImageView backView;
+    @BindView(R.id.id_iv_photo) ImageView photoView;
 
     @Override
     protected int getLayoutId() {
@@ -26,6 +32,17 @@ public class UserActivity extends BaseActivity{
     public void initView() {
 
         mImmersionBar.reset().transparentBar().init();
+
+        Glide.with(this).load("http://c.hiphotos.baidu.com/image/pic/item/f9198618367adab4b025268587d4b31c8601e47b.jpg")
+                .apply(new RequestOptions().error(getResources().getDrawable(R.mipmap.ic_launcher))
+                        .placeholder(R.mipmap.ic_launcher).centerCrop().diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .transform(new GlideCircleTransformWithBorder(this,2, Color.parseColor("#ccffffff")))
+                ).into(photoView);
+
+//        RequestOptions mRequestOptions = RequestOptions.circleCropTransform().diskCacheStrategy(DiskCacheStrategy.NONE)
+//                .skipMemoryCache(true).placeholder(R.mipmap.ic_launcher);
+//
+//        Glide.with(this).load("http://c.hiphotos.baidu.com/image/pic/item/f9198618367adab4b025268587d4b31c8601e47b.jpg").apply(mRequestOptions).into(photoView);
 
     }
 
